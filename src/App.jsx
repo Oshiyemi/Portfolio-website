e02156a13267b5f333507b9d34b0bf4b29ab6ef7
Oshiyemi Kashope <kashopeoshiyemi@gmail.com>
2026-02-28 15:44:20 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home";
 import Skills from "./pages/Skills/Skills";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
-import Contact from "./pages/Contact Us/ContactUs";
+import Contact from "./pages/ContactUs/ContactUs";
 import "./App.css";
 
 function App() {
